313a026e73d01d7f3d05298e4e655711ff61a99b
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-15 19:09:42 +0900

    バリデーションの設定と記述ミス修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :groups, through: :group_users
-  has_many :user_groups
+  has_many :group_users
 
 end
