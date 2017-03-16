51803032c0811b78856dbdd047661e8330a524fb
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-16 20:06:48 +0900

    groupsコントローラーにupdateアクションを追記

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,6 +20,17 @@ class GroupsController < ApplicationController
   def edit
   end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループが更新されました'
+    else
+      flash.now[:alert] = 'グループが更新されませんでした'
+      render action: :edit
+    end
+
+  end
+
   private
   def group_params
     params.require(:group).permit(:name)
