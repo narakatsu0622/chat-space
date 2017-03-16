7780339cf49e8c32d825f552408b2407c77ac5e8
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-16 14:08:16 +0900

    groups_controllerに追記

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,28 @@
 class GroupsController < ApplicationController
 
+  def new
+    @group = Group.new
+  end
+
   def index
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループが作成されました'
+    else
+      redirect_to new_group_path, alert: 'グループ名を入力してください'
+    end
   end
 
   def edit
   end
 
-  def new
+  private
+  def group_params
+    params.require(:group).permit(:name)
   end
+
 
 end
