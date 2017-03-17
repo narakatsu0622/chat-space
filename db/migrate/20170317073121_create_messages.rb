1bbb3a1f4c126a0f8db634da99e981c49d91f074
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-17 16:40:28 +0900

    マイグレーションファイルに定義を追記

---
+++ db/migrate/20170317073121_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.text :body, null:false
+      t.text :image
+      t.references :group_id, foreign_key: true
+      t.references :user_id,  foreign_key: true
+      t.timestamps
+    end
+  end
+end
