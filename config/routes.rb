a3f58589853caeef404fb970f5f5c22d024d8a61
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-20 18:05:09 +0900

    可読性向上のための記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "top#index"
   resources :groups, except: [:show,:destroy,:index] do
-    resources :messages, only: [:index,:create]
+    resources :messages, only: [:index, :create]
   end
 end
