78ee72713d1044e8c03ac9a59aa95af84f651bd4
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-20 16:37:28 +0900

    messagesコントローラーに追記、修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,13 +3,15 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @users = @group.users
     @messages = @group.messages
+    @message = Message.new
   end
 
   def create
     @message = Message.new(message_params)
-    if Message.save
+    if @message.save
       redirect_to group_messages_path, notice: 'メッセージが投稿されました'
     else
+      @group = Group.find(params[:group_id])
       flash.now[:alert] = 'メッセージを入力してください'
       render action: :index
     end
@@ -18,6 +20,6 @@ class MessagesController < ApplicationController
   private
   def message_params
     params[:user_id] = current_user.id
-    params.permit(:body, :image, :group_id, :user_id)
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
