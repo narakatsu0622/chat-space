062f22053c7c4ddf84d43244581430096d3d3d54
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-24 13:45:21 +0900

    非推奨の記述の修正

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -11,7 +11,7 @@ describe MessagesController do
 
   describe 'GET #index' do
     before :each do
-      get :index, group_id: group.id
+      get :index, params: { group_id: group.id }
     end
 
       it "assigns the requested contact @message" do
@@ -26,19 +26,19 @@ describe MessagesController do
   describe 'POST #create' do
     it "saves the new contact in the database" do
       expect {
-        post :create, { group_id: group.id, message: attributes_for(:message) }
+        post :create, params: { group_id: group.id, message: attributes_for(:message) }
       }.to change(Message, :count).by(1)
     end
 
     context 'create successful' do
       example ": create successful to render the :index template" do
-        post :create, { group_id: group.id, message: attributes_for(:message) }
+        post :create, params: { group_id: group.id, message: attributes_for(:message) }
         expect(response).to redirect_to(group_id: group.id)
       end
     end
     context 'create unsuccessful' do
       example ": create unsuccessful to render the same template" do
-        post :create, { group_id: group.id, message: invalid_attributes }
+        post :create, params: { group_id: group.id, message: invalid_attributes }
         expect(response).to render_template :index, group_id: group.id
       end
     end
