16e236c4bfd298f2dbb7aff9b1626f06a062297e
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-24 13:32:27 +0900

    外部キーの乱数化

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :message do
     body       { Faker::Lorem.sentence }
     image      ""
-    group_id   1
-    user_id    1
+    group_id   { Faker::Number.number(10)}
+    user_id    { Faker::Number.number(10)}
     created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 end
