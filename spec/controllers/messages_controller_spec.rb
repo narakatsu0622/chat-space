3d7763b213a62ca06af041ffee1120b2a458c97c
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-24 15:59:24 +0900

    インデント修正とフラッシュメッセージ表示のテスト追記

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -14,32 +14,49 @@ describe MessagesController do
       get :index, params: { group_id: group.id }
     end
 
-      it "assigns the requested contact @message" do
-        expect(assigns(:message)).to be_a_new(Message)
-      end
+    it "assigns the requested contact @message" do
+      expect(assigns(:message)).to be_a_new(Message)
+    end
 
-      it "renders the :index template" do
-        expect(response).to render_template :index
-      end
+    it "renders the :index template" do
+      expect(response).to render_template :index
+    end
   end
 
   describe 'POST #create' do
-    it "saves the new contact in the database" do
-      expect {
-        post :create, params: { group_id: group.id, message: attributes_for(:message) }
-      }.to change(Message, :count).by(1)
-    end
+    context 'with valid attributes' do
+      it "saves the new contact in the database" do
+        expect {
+          post :create, params: { group_id: group.id, message: attributes_for(:message) }
+        }.to change(Message, :count).by(1)
+      end
 
-    context 'create successful' do
-      example ": create successful to render the :index template" do
+      it " create successful to render the :index template" do
         post :create, params: { group_id: group.id, message: attributes_for(:message) }
         expect(response).to redirect_to(group_id: group.id)
       end
+
+      it 'shows notice a message' do
+        post :create, params: { group_id: group.id, message: attributes_for(:message) }
+        expect(flash[:notice]).to include('メッセージが投稿されました')
+      end
     end
-    context 'create unsuccessful' do
-      example ": create unsuccessful to render the same template" do
+
+    context 'with invalid attributes' do
+      it 'can not the new contact in the database' do
+        expect{
+          post :create, params: { group_id: group.id, message: invalid_attributes }
+          }.to change(Message, :count).by(0)
+      end
+
+      it " create unsuccessful to render the same template" do
         post :create, params: { group_id: group.id, message: invalid_attributes }
         expect(response).to render_template :index, group_id: group.id
+      end
+
+      it 'shows alert a message' do
+        post :create, params: { group_id: group.id, message: invalid_attributes }
+        expect(flash[:alert]).to include('メッセージを入力してください')
       end
     end
   end
