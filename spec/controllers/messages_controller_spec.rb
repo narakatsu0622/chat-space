299ccb592a7dca040518944b1605d185bff03d3d
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-03-24 13:28:30 +0900

    messagesコントローラテストの記法修正

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -10,31 +10,37 @@ describe MessagesController do
   end
 
   describe 'GET #index' do
-    it "assigns the requested contact @message" do
-        get :index, group_id: group.id
-        expect(assigns(:message)).to be_a_new(Message)
+    before :each do
+      get :index, group_id: group.id
     end
 
-    it "renders the :index template" do
-        get :index, group_id: group.id
-        expect(response).to render_template :index
-    end
+      it "assigns the requested contact @message" do
+          expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it "renders the :index template" do
+          expect(response).to render_template :index
+      end
   end
 
   describe 'POST #create' do
     it "saves the new contact in the database" do
       expect {
-        post :create, { group_id: group.id, message: attributes_for(:message) } }.to change(Message, :count).by(1)
+        post :create, { group_id: group.id, message: attributes_for(:message) }
+      }.to change(Message, :count).by(1)
     end
 
-    example ": create successful to render the :index template" do
-      post :create, { group_id: group.id, message: attributes_for(:message) }
-      expect(response).to redirect_to(group_id: group.id)
+    context 'create successful' do
+      example ": create successful to render the :index template" do
+        post :create, { group_id: group.id, message: attributes_for(:message) }
+        expect(response).to redirect_to(group_id: group.id)
+      end
     end
-
-    example ": create unsuccessful to render the same template" do
-      post :create, { group_id: group.id, message: invalid_attributes }
-      expect(response).to render_template :index, group_id: group.id
+    context 'create unsuccessful' do
+      example ": create unsuccessful to render the same template" do
+        post :create, { group_id: group.id, message: invalid_attributes }
+        expect(response).to render_template :index, group_id: group.id
+      end
     end
   end
 end
