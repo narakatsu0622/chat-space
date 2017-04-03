4057a163e706da5f5f83985e5fb7544895143fef
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-04-03 15:15:13 +0900

    インクリメンタルサーチ用のルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "top#index"
   resources :groups, except: [:show,:destroy,:index] do
-    collection do
-      get :ajax_user_list
-    end
     resources :messages, only: [:index, :create]
   end
+  resources :users, only: [:index]
 end
