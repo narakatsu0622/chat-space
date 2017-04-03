2f2e47dd3d67484ae449c3743efccdc354dfc3df
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-04-03 13:31:27 +0900

    ユーザーのインクリメンタルサーチ化用ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "top#index"
   resources :groups, except: [:show,:destroy,:index] do
+    collection do
+      get :ajax_user_list
+    end
     resources :messages, only: [:index, :create]
   end
 end
