d4a248e4c85d484c1829184ab9c33c0eed7c73dc
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-04-07 16:08:45 +0900

    imageのみの送信を可能にした

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
-  validates_presence_of :body
+  validates_presence_of :body, if: "image.blank?"
   mount_uploader :image, ImageUploader
 end
