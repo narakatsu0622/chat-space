b2a3240bf18f5d1e3dd27eff939bf1cd2e7d0a93
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-04-07 16:07:49 +0900

    ユーザーが所属しているグループをビューに反映

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
     @message = Message.new
+    @groups = current_user.groups
   end
 
   def create
