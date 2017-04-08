868bc0dcb70dba8d017f7da089804c2fc700ba46
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-04-08 15:28:39 +0900

    スコープを定義

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
   belongs_to :group
   validates_presence_of :body, if: "image.blank?"
   mount_uploader :image, ImageUploader
+  scope :after_id, -> (last_message_id) { where( "id > ?", last_message_id ) }
 end
