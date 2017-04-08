8b1e17159c7ab20176c381069c8f1e78f55ab569
narakatsu0622 <narakatsu0622+github@gmail.com>
2017-04-08 15:27:57 +0900

    自動更新のajaxを追記

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,13 @@
 $(function() {
   function buildHTML(message) {
-    var html = (`<div class="chat-space">
+    var html = (`<div class="ajax-chatspace" data-message_id=${message.id}>
                  <P class=chatspace-tweet-username>${ message.name }</p>
                  <p class=chatspace-tweet-time>${ message.created_at }</p>
                  <p class=chatspace-tweet-text>${ message.body }</p>
                  <image src= ${ message.image.url }</div>`);
     return html;
   }
+
 
   $('.js-form').on('submit', function(e) {
     e.preventDefault();
@@ -38,4 +39,28 @@ $(function() {
   $('#image_input').on("click", function() {
     $('#image_input_origin').click();
   });
+
+  var setIV = setInterval(function(){
+  var path_name = location.pathname;
+  var last_message_id = $('.ajax-chatspace').last().data('message_id');
+    if ( path_name.match( /messages/ )) {
+        $.ajax({
+          type: 'GET',
+          url: path_name,
+          data: { last_message_id: last_message_id },
+          dataType: 'json',
+        })
+        .done(function(data) {
+          $.each( data.update_message, function( i, message ) {
+            var html = buildHTML(message);
+            $('.chatspace').append(html);
+          });
+        })
+        .fail(function() {
+          alert('error');
+        });
+    } else {
+        clearInterval(setIV);
+      }
+    }, 1000);
 });
